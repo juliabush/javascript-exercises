8ddd042ea67be8d9d6ab19bc4aaedbab622c344d
juliabush <j.elizabethbush@gmail.com>
2025-03-03 16:15:51 +0100

    Completing the revereseString excercise from FEM

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversed = "";
 
-};
+    for (const c of str) {
+        reversed = c + reversed;
+    }
+    return reversed;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
